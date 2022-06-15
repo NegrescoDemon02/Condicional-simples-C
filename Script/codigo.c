1e9f4281a603b657553e4eff5caad8f029978e59
Kiara Victoria <83596877+NegrescoDemon02@users.noreply.github.com>
2022-06-14 23:17:59 -0300

    atualização01
    com erro

---
+++ Script/codigo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(int argc, char const *argv[])
+{
+    int num;
+    float raiz;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &num);
+    if(num % 2 == 0){
+        raiz = sqrt("A raiz quadrada e: %.2f", raiz);
+        }
+    return 0;
+}
